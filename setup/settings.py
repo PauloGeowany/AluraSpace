a5981d8a1e71e80de033f6813b44f1ec4dddbb34
Paulo <paulogeodias@academico.ufs.br>
2025-09-29 17:01:37 -0300

    adicionando a galeria e a exibição com o http

--- setup/settings.py
+++ setup/settings.py
@@ -10,10 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-from pathlib import Path, os
+from pathlib import Path
+import os;
 from dotenv import load_dotenv 
 
 load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'galeria',
 ]
 
 MIDDLEWARE = [
