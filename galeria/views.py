5389f4512d461714573ddce9f0c0c75853354272
Paulo <paulogeodias@academico.ufs.br>
2025-10-01 01:11:02 -0300

    .

--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h1>ALURA SPACE</h1>' \
-    '<p>Seja bem vindo ao Espaço!</p>')
+    return render(request, 'index.html')
 
 
